8d29405083e43e622d5e9e381a4f1c462331e496
kaianzx7 <114822035+kaianzx7@users.noreply.github.com>
2022-11-13 13:37:13 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+let nome= prompt("Digite seu nome: " )
+let Ano= prompt("Digite o ano que você nasceu: ")
+let Anoatual= prompt("Digite o ano atual: ")
+ 
+let idade = Anoatual - Ano
+
+
+document.write(`Olá, seu nome é ${nome} e você atualmente tem ${idade} anos!`)
